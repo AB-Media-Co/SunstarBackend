c44562b6f782b0ff2be77f0770e5dc547803a9f9
Prakash-b1 <pinnub23@gmail.com>
2025-02-04 15:47:16 +0530

    updated vercel

--- server.js
+++ server.js
@@ -43,11 +43,14 @@ app.use('/api/instagram', instagramRoutes); // Use the Instagram routes
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Serve static files from the build folder
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Catch-all route to serve index.html (for client-side routing support)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

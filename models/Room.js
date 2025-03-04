c7ffe0d738c38fa68e6b0a68151227b673f724b0
Prakash-b1 <pinnub23@gmail.com>
2025-03-04 15:19:42 +0530

    Updated coode wth changes

--- models/Room.js
+++ models/Room.js
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 
 const RoomSchema = new mongoose.Schema({
-  RoomTypeID: { type: String, required: true, unique: true },
-  RateTypeID: { type: String, required: true, unique: true },
+  RoomTypeID: { type: String, required: true },
+  RateTypeID: { type: String, required: true },
   RoomImage: [{ type: String }],
   HotelCode: { type: String },
   RoomName: { type: String, required: true },
   RoomDescription: { type: String },
+  FromDate: { type: String },
+  ToDate: { type: String },
   AboutRoom: {
     description: { type: String },
     img: { type: String },
@@ -21,5 +23,7 @@ const RoomSchema = new mongoose.Schema({
   squareFeet: { type: Number },
   available: { type: Boolean },
 });
+
+RoomSchema.index({ RoomTypeID: 1, RateTypeID: 1 }, { unique: true });
 
 export default mongoose.model('Room', RoomSchema);
